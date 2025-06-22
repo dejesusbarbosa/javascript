63c268e1e04e1c264dc10a8ea8ef8dc365d07db3
Lucas de jesus <sopvervideolucas@gmail.com>
2025-06-22 17:45:13 -0300

    estudando if else (js)

---
+++ guanabara/ex-008.js
@@ -0,0 +1,8 @@
+let vel = 60.5
+console.log(` Á velocidade do seu carro é ${vel} km!`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+} else {
+    console.log(`Dirija sempre usando cinto de segurança!`)
+}
---
+++ guanabara/ex-009.js
@@ -0,0 +1,8 @@
+let país = 'Brasil'
+
+console.log(`Você está vivendo no ${país}`)
+if (país == 'Brasil') {
+    console.log('Você é brabo')
+} else {
+    console.log('Você é muito paia mané, feião')
+}
